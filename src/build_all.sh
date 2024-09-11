9a178f9e31b2fa076e9d694aee8c345ec0fd416b
Ovidio Peña Rodríguez <ovidiopr@users.noreply.github.com>
2024-09-11 13:45:07 +0200

    Updated build

--- src/build_all.sh
+++ src/build_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-lazbuild --operating-system=linux --cpu=x86_64 --widgetset=gtk2 Digit.lpi --build-mode=Release
-lazbuild --operating-system=win32 --cpu=i386 --widgetset=win32 Digit.lpi --build-mode=Release
-lazbuild --operating-system=win64 --cpu=x86_64 --widgetset=win32 Digit.lpi --build-mode=Release
+lazbuild --operating-system=linux --cpu=x86_64 --widgetset=gtk2 digit.lpi --build-mode=Release
+lazbuild --operating-system=win32 --cpu=i386 --widgetset=win32 digit.lpi --build-mode=Release
+lazbuild --operating-system=win64 --cpu=x86_64 --widgetset=win32 digit.lpi --build-mode=Release
